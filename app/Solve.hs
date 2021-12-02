e98a595b5d22ca11fba488b2ac7f64341a068c60
Bob Klaase <>
2021-12-02 21:53:49 +0100

    tweaks to tests

--- app/Solve.hs
+++ app/Solve.hs
@@ -4,6 +4,7 @@ import System.IO
 import Text.Read
 import qualified AoC.Lib as L
 import Solutions.Day1 (day1)
+import Solutions.Day2 (day2)
 
 main :: IO ()
 main = 
@@ -17,6 +18,7 @@ solve :: Int -> IO ()
 solve n =
   case n of
     1 -> L.solve day1
+    2 -> L.solve day2
     otherwise -> putStrLn "no solution found..."
 
 askForType :: Read a => IO a
--- test/Solutions/Day1Spec.hs
+++ test/Solutions/Day1Spec.hs
@@ -6,9 +6,9 @@ import Solutions.Day1
 test_input_day1_1 :: [Int]
 test_input_day1_1 = [199,200,208,210,200,207,240,269,260,263]
 
-spec = context "part1" $ do
-  it "should calculate output for part 1" $
+spec = context "Day 1" $ do
+  it "should calculate test output for part 1" $
     part1 test_input_day1_1 `shouldBe` 7
 
-  it "should calculate output for part 2" $
+  it "should calculate test output for part 2" $
     part2 test_input_day1_1 `shouldBe` 5
